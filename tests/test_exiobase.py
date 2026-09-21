4ff6fd8c9d043bc8b59c158015c6d185254e22ac
IngiRG <igasadal@gmail.com>
2026-09-21 12:08:38 +0100

    Test full MRIO price orientation and embodied labor

--- tests/test_exiobase.py
+++ tests/test_exiobase.py
@@ -1,5 +1,6 @@
 import numpy as np, pandas as pd
-from ewa.exiobase import construct_counterfactual,counterfactual_prices
+from ewa.exiobase import construct_counterfactual
+from ewa.full_mrio import solve_unit_costs,price_residual,embodied_by_origin_destination
 
 def test_counterfactual_conserves_compensation():
     idx=pd.MultiIndex.from_tuples([("AAA","s1"),("BBB","s1")])
@@ -12,6 +13,16 @@ def test_counterfactual_conserves_compensation():
 def test_counterfactual_price_identity():
     A=np.array([[.2,.1],[.1,.2]])
     la=np.array([.4,.4]); le=np.array([.5,.3]); nl=np.array([.2,.2])
-    pa,pe=counterfactual_prices(A,la,le,nl)
-    assert np.allclose(pa,A@pa+la+nl)
-    assert np.allclose(pe,A@pe+le+nl)
+    pa=solve_unit_costs(A,la+nl); pe=solve_unit_costs(A,le+nl)
+    assert np.allclose(pa,A.T@pa+la+nl)
+    assert np.allclose(pe,A.T@pe+le+nl)
+    assert price_residual(A,pa,la+nl)<1e-12
+
+def test_embodied_factor_conserves_simple_final_demand():
+    A=np.zeros((2,2))
+    idx=pd.MultiIndex.from_tuples([("AA","s"),("BB","s")])
+    cols=pd.MultiIndex.from_tuples([("AA","fd"),("BB","fd")])
+    Y=pd.DataFrame([[2.,3.],[5.,7.]],index=idx,columns=cols)
+    out=embodied_by_origin_destination(A,Y,np.array([10.,20.]),np.array(["AA","BB"]))
+    assert np.isclose(out.loc["AA","BB"],30.)
+    assert np.isclose(out.loc["BB","AA"],100.)
